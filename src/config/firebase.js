d49284b15f8fc008dcfd19d7ab6810ce496019d7
togami <tuabobo123@gmail.com>
2020-06-01 14:45:54 +0900

    add firebaseConfig

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqB6OrPg-dRLH9TIEMNIjR8S_EvgDDMsw",
+  authDomain: "chatapp-b1aff.firebaseapp.com",
+  databaseURL: "https://chatapp-b1aff.firebaseio.com",
+  projectId: "chatapp-b1aff",
+  storageBucket: "chatapp-b1aff.appspot.com",
+  messagingSenderId: "109696326222",
+  appId: "1:109696326222:web:a461f8d23b885046ea011f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
